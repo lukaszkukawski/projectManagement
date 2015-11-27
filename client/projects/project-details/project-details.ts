201de2f8d84baae334a826631b0f43453abec286
kuka <kukawski.lukasz@gmail.com>
2015-11-27 19:00:07 +0100

    Add support for sass, bootsrap, ec6-promise. And add first component for project list

---
+++ client/projects/project-details/project-details.ts
@@ -0,0 +1,15 @@
+/// <reference path="../../../typings/angular2-meteor.d.ts" />
+import {Component, View} from 'angular2/angular2';
+
+@Component({
+    selector: 'project-details'
+})
+@View({
+    templateUrl: './projects/project-details/project-details.html',
+    directives: []
+})
+export class ProjectDetails {
+    constructor() {
+
+    }
+}
